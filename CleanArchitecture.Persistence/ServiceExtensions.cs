5f98f18efb2e4613336bd8b57eda1bf08a02baf6
Wesley Goulart <wesley.goulart@totvs.com.br>
2025-06-22 00:19:52 -0300

    Criando repositories e string de conexão

---
+++ CleanArchitecture.Persistence/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence
+{
+  public static class ServiceExtensions
+  {
+    public static void ConfigurePersistenceApp(this IServiceCollection services, 
+                                            IConfiguration configution)
+    {
+      string connectionString = configution.GetConnectionString("Sqlite");
+      services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connectionString));
+
+      services.AddScoped<IUnitOfWork, UnitOfWork>();
+      services.AddScoped<IUserRepository, UserRepository>();
+    }
+  }
+}
